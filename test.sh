f14067dc7cbf45a38e52260b9ec8adf5dee219dd
Manoj <4manojverma@gmail.com>
2022-05-14 20:14:56 -0700

    shell utils

--- test.sh
+++ test.sh
@@ -1,3 +1,27 @@
+#! /bin/bash
+
 ./scripts/clrit.sh -p "config|newton|error|fail|warn|running|some|normal" ./test.log
 
 cat ./test.log | ./scripts/clrit.sh -p "config|newton|error|fail|warn|running|some|normal"
+
+
+. ./scripts/shell_utils.sh
+msg "no color used"
+msg "BPurple color used" "$BPurple"
+msg "no color used"
+echo ""
+msgln "no color used"
+msglnbox "red color used" "$Red"
+msgln "no color used"
+
+boxedmessagetest
+_set_box_chr '-'
+boxedmessagetest
+_set_box_chr '_'
+boxedmessagetest
+_set_box_chr '*'
+boxedmessagetest
+_set_box_chr '~'
+boxedmessagetest
+_set_box_chr '─'
+boxedmessagetest
